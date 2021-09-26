5fc85f8cc768554edd5923377aa51ea9d8f0fd3b
Ruslan1493 <star93@list.ru>
2021-09-26 22:59:10 +0300

    adding express cube workshop

---
+++ config/express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+
+    //TODO: Setup the view engine
+    app.engine('hbs', handlebars());
+    app.set('view engine', 'hbs');
+    //TODO: Setup the body parser
+    // app.use(bodyParser.json())
+    // app.use(express.json());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    //TODO: Setup the static files
+    app.use(express.static('static'))
+};
